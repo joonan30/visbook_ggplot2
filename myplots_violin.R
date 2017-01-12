a5b3c619780a8447a0a60c9fdc6a20bec156fb2d
joonan30 <joon.an30@gmail.com>
2017-01-11 22:19:57 -0800

    Updates

--- myplots_violin.R
+++ myplots_violin.R
@@ -1,41 +1,14 @@
 violin_2vars <- function(data, var_x, factor_a, title, x_title, y_title){
-  p <- ggplot(data, aes(x=var_x, color=factor_a, fill=factor_a)) +
-  geom_histogram(aes(y=..density..), position="identity", alpha=0.5, bins=200) + 
-  scale_color_manual(values=c("firebrick", "steelblue")) + scale_fill_manual(values=c("firebrick", "steelblue")) +
-  labs(title=title,x=x_title, y = y_title) +
-  theme_bw()
+  p <- ggplot(data, aes(factor_a, (var_x))) + geom_violin(aes(fill=factor_a)) + 
+  scale_fill_manual(values=c("firebrick", "steelblue")) + labs(title=title,x=x_title, y = y_title) +
+  theme_light()
   return (p)
 }
 
 violin_2vars_log10 <- function(data, var_x, factor_a, title, x_title, y_title){
-    p <- ggplot(data, aes(x=log10(var_x), color=factor_a, fill=factor_a)) +
-      geom_histogram(aes(y=..density..), position="identity", alpha=0.5, bins=200) + 
-      scale_color_manual(values=c("firebrick", "steelblue")) + scale_fill_manual(values=c("firebrick", "steelblue")) +
-      labs(title=title,x=x_title, y = y_title) +
+  p <- ggplot(data, aes(factor_a, log10(var_x))) + geom_violin(aes(fill=factor_a)) + 
+    scale_fill_manual(values=c("firebrick", "steelblue")) + labs(title=title,x=x_title, y = y_title) +
+    theme_light()
       theme_bw()
-  return (p)
-}
-
-violin_2vars_density <- function(data, var_x, factor_a, title, x_title, y_title){
-  require(plyr)
-  mu <- ddply(data, factor_a, summarise, grp.mean=mean(var_x))
-  p <- ggplot(data, aes(x=var_x, color=factor_a, fill=factor_a)) +
-    geom_histogram(aes(y=..density..), position="identity", alpha=0.5, bins=200) + 
-    geom_density(alpha=0.6) + geom_vline(data=mu, aes(xintercept=grp.mean, color=factor_a), linetype="dashed") +
-    scale_color_manual(values=c("firebrick", "steelblue")) + scale_fill_manual(values=c("firebrick", "steelblue")) +
-    labs(title=title,x=x_title, y = y_title) +
-    theme_bw()
-  return (p)
-}
-
-violin_2vars_log10_density <- function(data, var_x, factor_a, title, x_title, y_title){
-  require(plyr)
-  mu <- ddply(data, factor_a, summarise, grp.mean=mean(log10(var_x)))
-  p <- ggplot(data, aes(x=log10(var_x), color=factor_a, fill=factor_a)) +
-    geom_histogram(aes(y=..density..), position="identity", alpha=0.5, bins=200) + 
-    geom_density(alpha=0.6) + geom_vline(data=mu, aes(xintercept=grp.mean, color=factor_a), linetype="dashed") +
-    scale_color_manual(values=c("firebrick", "steelblue")) + scale_fill_manual(values=c("firebrick", "steelblue")) +
-    labs(title=title,x=x_title, y = y_title) +
-    theme_bw()
   return (p)
 }
